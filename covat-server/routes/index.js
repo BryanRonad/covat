3b0027c0f8c4b9fb561114a013578855337521d2
BryanRonad <bryanronad@gmail.com>
2022-04-13 02:03:04 +0530

    API created for insertion

--- covat-server/routes/index.js
+++ covat-server/routes/index.js
@@ -1,9 +1,26 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var admin = require("firebase-admin");
+var serviceAccount = require("../covat-3bd0b-firebase-adminsdk-e2vx0-5106130756.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL:
+    "https://covat-3bd0b-default-rtdb.asia-southeast1.firebasedatabase.app",
+});
+var db = admin.database();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.post("/insert", (req, res) => {
+  console.log(req.body);
+  const recordToPush = req.body;
+  recordToPush.datetime = new Date().toLocaleString();
+  db.ref("records").set(recordToPush);
+  res.status(200).send("Attendance record added.");
 });
 
 module.exports = router;
