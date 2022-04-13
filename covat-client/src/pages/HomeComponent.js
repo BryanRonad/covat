dcab92ea4b0ce7f9ce88b2f321dc8e56db3652ca
BryanRonad <bryanronad@gmail.com>
2022-04-13 09:21:17 +0530

    User info modal created

--- covat-client/src/pages/HomeComponent.js
+++ covat-client/src/pages/HomeComponent.js
@@ -11,6 +11,21 @@ import {
   TableCaption,
   Box,
   useToast,
+  Button,
+  useDisclosure,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  ModalHeader,
+  ModalContent,
+  ModalOverlay,
+  Modal,
+  VStack,
+  Text,
+  Heading,
+  HStack,
+  Image,
+  Center,
 } from '@chakra-ui/react';
 import {
   onChildAdded,
@@ -28,7 +43,9 @@ function HomeComponent() {
   const navigate = useNavigate();
   const [auth, setAuth] = useState(null);
   const [tableData, setTableData] = useState([]);
+  const [modalInfo, setModalInfo] = useState({});
   const toast = useToast();
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   useEffect(() => {
     let authToken = sessionStorage.getItem('Auth Token');
@@ -83,20 +100,71 @@ function HomeComponent() {
                   <Th>DEPARTMENT</Th>
                   <Th>SEMESTER</Th>
                   <Th>TEMPERATURE</Th>
+                  <Th>INFO</Th>
                 </Tr>
               </Thead>
               {tableData.length > 0 && (
                 <Tbody>
                   {tableData.map((ele, index) => (
-                    <Tr key={index}>
+                    <Tr
+                      key={index}
+                      color={ele.temperature > 100.4 && 'red.400'}
+                    >
                       <Td>{ele.name}</Td>
                       <Td>{ele.rno}</Td>
                       <Td>{ele.datetime}</Td>
                       <Td>{ele.department}</Td>
                       <Td>{ele.semester}</Td>
                       <Td>{ele.temperature}</Td>
+                      <Td>
+                        <Button
+                          onClick={e => {
+                            onOpen();
+                            setModalInfo(ele);
+                          }}
+                        >
+                          View info
+                        </Button>
+                      </Td>
                     </Tr>
                   ))}
+                  <Modal isOpen={isOpen} onClose={onClose} size="xl">
+                    <ModalOverlay />
+                    <ModalContent>
+                      <ModalHeader>User Info</ModalHeader>
+                      <ModalCloseButton />
+                      <ModalBody>
+                        <VStack>
+                          <Box>
+                            <HStack>
+                              <Text>Name:</Text>
+                              <Heading size="md">{modalInfo.name}</Heading>
+                            </HStack>
+                          </Box>
+                          <Box>
+                            <HStack>
+                              <Text>Roll no.:</Text>
+                              <Heading size="md">{modalInfo.rno}</Heading>
+                            </HStack>
+                          </Box>
+                          <Box>
+                            <Center>
+                              <Image
+                                boxSize="max-content"
+                                src={`data:image/png;base64,${modalInfo.img}`}
+                              />
+                            </Center>
+                          </Box>
+                        </VStack>
+                      </ModalBody>
+
+                      <ModalFooter>
+                        <Button colorScheme="blue" mr={3} onClick={onClose}>
+                          Close
+                        </Button>
+                      </ModalFooter>
+                    </ModalContent>
+                  </Modal>
                 </Tbody>
               )}
             </Table>
